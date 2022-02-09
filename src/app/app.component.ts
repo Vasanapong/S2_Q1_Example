1ea255ef392b842ebe4d9565d2d9359aa8c17e0b
Vasanapong <vasanapong.c@gmail.com>
2022-02-10 04:07:54 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 's2_q1_ex';
+
+  calculateMode = 'isPrime'
+  checkValidNumber = false
+  numberInput = 0
+
+  handleNumberChange(e: any) {
+    if(e.target.value > -1){
+      this.numberInput = e.target.value
+    }else{
+      this.numberInput = 1
+    }
+    this.checkValid(this.numberInput, this.calculateMode)
+  }
+
+  handleModeChange(e: any) {
+    this.calculateMode = e.target.value
+    this.checkValid(this.numberInput, this.calculateMode)
+  }
+
+  // Calculate Prime
+  isPrime = (num: number): any => {
+    if (Number(num) && num > 0) {
+      for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if (num % i === 0) return false;
+      return num > 1;
+    }
+  }
+
+  // Calculate Fibonacci
+  isPerfectSquare(x: number) {
+    let s = parseInt(Math.sqrt(x).toString());
+    return (s * s === x);
+  }
+
+  isFibonacci(n: number) {
+    return this.isPerfectSquare(5 * n * n + 4) ||
+      this.isPerfectSquare(5 * n * n - 4);
+  }
+
+  checkValid = (numberInput: number, calculateMode: String) => {
+    if (calculateMode === 'isPrime') {
+      let checked = this.isPrime(numberInput)
+      console.log(checked)
+      this.checkValidNumber = checked
+    }
+    if(calculateMode === 'isFibonacci'){
+      let checked = this.isFibonacci(numberInput)
+      this.checkValidNumber = checked
+    }
+  }
+
 }
